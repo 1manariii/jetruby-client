47860d0846fbde8d810be5adfc728854319522dc
imanariii <imanariii>
2025-03-04 02:27:26 +0300

    Добавил подсказку в интервейсе карточек репозиториев

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,8 @@
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import StarIcon from '@mui/icons-material/Star';
 import VisibilityIcon from '@mui/icons-material/Visibility';
-import {Button, Chip, Stack} from "@mui/material";
+import {Button, Chip, Stack, Tooltip} from "@mui/material";
+import React from "react";
 
 const cardStyle = {
     color: 'white',
@@ -44,7 +45,9 @@ const Card = ({repository}) => {
         <>
             <div style={cardStyle}>
                 <span style={numberForCard}>№ {id}</span>
-                <a href={`https://github.com/${owner}/${name}`}><Chip label={name} color="secondary" variant="contained" /></a>
+                <Tooltip title="Нажмите, чтобы открыть репозиторий на GitHub">
+                    <a href={`https://github.com/${owner}/${name}`}><Chip label={name} color="secondary" variant="contained" /></a>
+                </Tooltip>
                 <Stack width="100%" alignItems="center" gap="1rem" justifyContent="space-between" flexDirection="row"><AccountBoxIcon /> {owner}</Stack>
                 <Stack width="100%" alignItems="center" gap="1rem" justifyContent="space-between" flexDirection="row"><StarIcon /> {stars}</Stack>
                 <Stack width="100%" alignItems="center" gap="1rem" justifyContent="space-between" flexDirection="row"><VisibilityIcon /> {watch}</Stack>
